b63993b3f2f9cc878a142a88becf80c2d90b68c1
Uday <budayteja009@gmail.com>
2022-03-21 20:38:42 +0530

    Added codes

---
+++ socialapp/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const dateFormat = require('date-format');
+
+const app = express();
+
+const PORT = 4000 || process.env.PORT
+
+app.get("/", (req, res) => {
+    res.status(200).send("<h1> Hello World </h1>")
+})
+
+app.get("/api/v1/instagram", (req, res) => {
+    const instaSocial = {
+        username: "dummy",
+        followers: "none",
+        follows: "none",
+        data: dateFormat.asString("dd[MM] - hh:mm:ss", new Date())
+    };
+
+    res.status(200).json({instaSocial});
+});
+
+app.get("/api/v1/:token", (req, res) => {
+    console.log(req.params.token);
+    res.status(200).json({param: req.params.token});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is active...`);
+})
+
